9429a124eb5316eb50e37f6ed2a94007e94aa62d
Lorenz Minder <lorenz@bitripple.com>
2025-03-07 10:58:24 -0600

    scripts:  Move to scripts/local.
    This is in preparation of adding another set of scripts to work on a two
    machine setup.  (scripts/local remains for single machine testing)
    Also, remove redundant files that can be reused from test/ and use those
    instead.

--- scripts/local/server.sh
+++ scripts/local/server.sh
@@ -5,4 +5,4 @@ set -e
 export LW_DANGEROUSLY_DISABLE_PERMISSIONS_CHECKS=1
 ip netns exec lightway-server \
 	./target/debug/lightway-server \
-	--config-file './scripts/server_config/server_config.yaml'
+	--config-file './scripts/local/server_config.yaml'
