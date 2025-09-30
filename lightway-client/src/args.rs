99ed88f870b8be5ac0cb72a6066581bcf323d28d
Felipe Flores <lffm.fismat@gmail.com>
2025-09-29 21:26:22 -0600

    fix, feat: In order for bitripple factory codec to be provided by config file, we needed to introduce the `inside_pkt_codec` field on `ConnectionConfig` as well as deserialization logic that can be extended to other codec providers. This is ultimately passed to the client through the `make_client_connection_config` function in such a way so that every server can independently specify their own codec to use.

--- lightway-client/src/args.rs
+++ lightway-client/src/args.rs
@@ -5,9 +5,10 @@ use bytesize::ByteSize;
 use clap::Parser;
 use lightway_app_utils::args::{Cipher, ConnectionType, Duration, LogLevel};
 use lightway_core::{AuthMethod, MAX_OUTSIDE_MTU};
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 use std::{net::Ipv4Addr, path::PathBuf};
 use twelf::config;
+use bitripple_factory_thin_wrapper::TunnelArgs;
 
 #[config]
 #[derive(Parser, Debug)]
@@ -186,6 +187,15 @@ impl Config {
     }
 }
 
+/// Codec configuration for inside packet encoding
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum CodecConfig {
+    /// BitRipple codec with tunnel arguments
+    #[serde(rename = "bitripple")]
+    BitRipple { tunnel_args: TunnelArgs },
+}
+
 #[config]
 #[derive(Parser, Debug, Serialize)]
 pub struct ConnectionConfig {
@@ -203,6 +213,11 @@ pub struct ConnectionConfig {
     /// Cipher to use for encryption
     #[serde(default)]
     pub cipher: Cipher,
+
+    /// Inside packet codec configuration
+    #[clap(skip)]
+    #[serde(default)]
+    pub inside_pkt_codec: Option<CodecConfig>,
 }
 
 #[cfg(test)]
